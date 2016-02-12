6e5f74382610173544f880d23cb623977ec43c62
tanner466 <tanner466@gmail.com>
2016-02-12 16:09:04 -0500

    Implement win game alert.

--- ttt.js
+++ ttt.js
@@ -1,7 +1,7 @@
 $(document).on('ready', function(){
   var turn = 0;
-
-  //var winningCombos =
+  var xSquares = [];
+  var oSquares = [];
 
   $('td').on('click', function(){
     // this === DOM element
@@ -12,23 +12,62 @@ $(document).on('ready', function(){
     var self = $(this);
 
     if ( turn % 2 ) {
-      self.html('O').addClass('o');
+      self.html('O');
+      oSquares.push(parseInt(self[0].id.substr(5)));
+      checkForWinner("o");
+
     }
 
     else {
-      self.html('X').addClass('x');
+      self.html('X');
+      xSquares.push(parseInt(self[0].id.substr(5)));
+      checkForWinner("x");
     }
 
     self.off('click');
 
-    //checkForWinner();
 
     turn++;
 
   });
 
-  // function checkForWinner() {
-  //   $('.x')
-  // }
+  function checkForWinner(whichSquares) {
+    if (whichSquares === 'x') {
+      console.log("X: " + xSquares);
+      if (xSquares.length > 2) {
+        for (var i=0; i<xSquares.length-2; i++) {
+          for (var j=i+1; j<xSquares.length-1; j++) {
+            for (var k=j+1; k<xSquares.length; k++) {
+              console.log("ADD X: " + xSquares[i] + ", " + xSquares[j] + ", " + xSquares[k] );
+              if (xSquares[i] + xSquares[j] + xSquares[k] === 15) {
+                console.log("WINNER");
+                $('td').off('click');
+                alert("X's win the game!!");
+              }
+            }
+          }
+        }
+      }
+
+    }
+    else {
+      console.log("O: " + oSquares);
+      if (oSquares.length > 2) {
+        for (var i=0; i<oSquares.length-2; i++) {
+          for (var j=i+1; j<oSquares.length-1; j++) {
+            for (var k=j+1; k<oSquares.length; k++) {
+              console.log("ADD O: " + oSquares[i] + ", " + oSquares[j] + ", " + oSquares[k] );
+              if (oSquares[i] + oSquares[j] + oSquares[k] === 15) {
+                console.log("WINNER");
+                $('td').off('click');
+                alert("O's win the game!!");
+              }
+            }
+          }
+        }
+      }
+
+    }
+  }
 
 });
