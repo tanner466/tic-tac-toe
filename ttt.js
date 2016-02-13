c02e337f675d4b248bca40a0e9b4b97c4bf9f7ba
tanner466 <tanner466@gmail.com>
2016-02-12 21:44:49 -0500

    Keep score and play new games.

--- ttt.js
+++ ttt.js
@@ -3,33 +3,31 @@ $(document).on('ready', function(){
   var xSquares = [];
   var oSquares = [];
 
-  $('td').on('click', function(){
-    // this === DOM element
-    // $(this) === jQuery object
+  $('td').on('click', clickSquare);
 
-    // Idempotent = Always get the same outcome
-    // Memoization = Memorize the result and store it
+  function clickSquare() {
     var self = $(this);
 
     if ( turn % 2 ) {
       self.html('O');
       oSquares.push(parseInt(self[0].id.substr(5)));
       checkForWinner("o");
-
+      $('#turn').html("X");
     }
 
     else {
       self.html('X');
       xSquares.push(parseInt(self[0].id.substr(5)));
       checkForWinner("x");
+      $('#turn').html("O");
+
     }
 
     self.off('click');
 
 
     turn++;
-
-  });
+  }
 
   function checkForWinner(whichSquares) {
     if (whichSquares === 'x') {
@@ -41,8 +39,11 @@ $(document).on('ready', function(){
               console.log("ADD X: " + xSquares[i] + ", " + xSquares[j] + ", " + xSquares[k] );
               if (xSquares[i] + xSquares[j] + xSquares[k] === 15) {
                 console.log("WINNER");
+                var currentXScore = parseInt($('#x_score').html());
+                $('#x_score').html(currentXScore + 1);
                 $('td').off('click');
                 alert("X's win the game!!");
+                return;
               }
             }
           }
@@ -59,8 +60,11 @@ $(document).on('ready', function(){
               console.log("ADD O: " + oSquares[i] + ", " + oSquares[j] + ", " + oSquares[k] );
               if (oSquares[i] + oSquares[j] + oSquares[k] === 15) {
                 console.log("WINNER");
+                var currentYScore = parseInt($('#y_score').html());
+                $('#y_score').html(currentYScore + 1);
                 $('td').off('click');
                 alert("O's win the game!!");
+                return;
               }
             }
           }
@@ -68,6 +72,30 @@ $(document).on('ready', function(){
       }
 
     }
+
+    if (turn===8) {
+      alert("Tie game!");
+      var currentDrawScore = parseInt($('#draw_score').html());
+      $('#draw_score').html(currentDrawScore + 1);
+    }
   }
+
+  $("#newGame").on('click', function() {
+
+    var squares = $('td');
+    squares.on('click', clickSquare);
+
+    for (var i=0; i<squares.length; i++) {
+      $("#cell-"+(i+1)).html(i+1);
+    }
+
+    $('#turn').html("X");
+
+
+    turn = 0;
+    xSquares = [];
+    oSquares = [];
+
+  });
 
 });
